ba44255864a87a3d5094af9efdbc29283b9534be
schulerdesigns <160165853+schulerdesigns@users.noreply.github.com>
2024-09-04 11:55:20 -0400

    added multiple functions for math operations

--- Lab215-3/Lab215-3.cpp
+++ Lab215-3/Lab215-3.cpp
@@ -5,9 +5,10 @@
 using namespace std;
 void printMenu(int& choice) {
 	cout << "Welcome to my program, please select an operation to perform:" << endl;
-	cout << "1. Placeholder" << endl;
-	cout << "2. Another Placeholder" << endl;
-	cout << "3. And a third one" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4: Division" << endl;
 	cout << "\nYour Selection: ";
 	cin >> choice;
 }
@@ -16,14 +17,27 @@ void getChoices(float& A, float& B) {
 	cin >> A;
 	cout << "Please enter the second value:";
 	cin >> B;
-	// The rest of this function is an exercise to the reader
 }
+//ADD
 void firstChoice(float A, float B) {
-	cout << "This is proving the first choice ran" << endl;
+	cout << A << "+" << B << " = " << A + B  << endl;
 }
+
+//SUB
 void secondChoice(float A, float B) {
-	cout << "This is proving the second choice ran" << endl;
+	cout << A << "-" << B << " = " << A - B << endl;
 }
+
+//MULT
+void thirdChoice(float A, float B) {
+	cout << A << "*" << B << " = " << A * B << endl;
+}
+
+//DIV
+void fourthChoice(float A, float B) {
+	cout << A << "/" << B << " = " << A / B << endl;
+}
+
 int main() {
 	int choice;
 	float A;
@@ -35,6 +49,12 @@ int main() {
 	}
 	if (choice == 2) {
 		secondChoice(A, B);
+	}
+	if (choice == 3) {
+		thirdChoice(A, B);
+	}
+	if (choice == 4) {
+		fourthChoice(A, B);
 	}
 	return 0;
 }
